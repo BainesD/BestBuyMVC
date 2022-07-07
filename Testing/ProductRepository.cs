ce0ff12ab180742730c7fbb9815bf21e5562a425
Baines Deal <bainesdeal@gmail.com>
2022-07-07 11:33:17 -0500

    added the ability to update individual product name, price, and stock levels

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -7,7 +7,7 @@ namespace Testing
 {
     public class ProductRepository : IProductRepository
     {
-       public ProductRepository(IDbConnection conn )
+        public ProductRepository(IDbConnection conn)
         {
             _conn = conn;
         }
@@ -19,6 +19,11 @@ namespace Testing
         public Product GetProduct(int id)
         {
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+        }
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price, StockLevel = @stocklevel WHERE ProductID = @id",
+                 new { name = product.Name, price = product.Price, stocklevel = product.StockLevel, id = product.ProductID });
         }
 
     }
